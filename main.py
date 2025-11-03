34efd54a49c73f5654b929855e6f9d1cea383b7c
Raphaël <twizox@gmail.com>
2025-11-03 14:37:00 +0100

    fix test workflow

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 from __future__ import annotations
 import pygame as pg
-import sys 
-
+import sys
 
 from camera import Camera
 from map import *
 from player import *
-RES = WIDTH, HEIGHT = 1080,720
+
+RES = WIDTH, HEIGHT = 1080, 720
 TILE_SIZE = 64
 NUMBER_OF_TILES_X, NUMBER_OF_TILES_Y = WIDTH // TILE_SIZE, HEIGHT // TILE_SIZE
 FPS = 60
 
+
 class Game:
     """Main Game class to handle game initialization, updates, and rendering."""
+
     def __init__(self) -> None:
         pg.init()
         self.screen = pg.display.set_mode(RES, pg.RESIZABLE)
-        self.clock = pg.time.Clock() 
+        self.clock = pg.time.Clock()
         self.delta_time = 1
         self.current_res = self.screen.get_size()
         self.half_width = self.current_res[0] // 2
@@ -29,13 +31,13 @@ class Game:
         self.player = Player(self, (0, 0), 2)
         self.camera = Camera(self)
 
-    def update(self): 
+    def update(self):
         self.player.update()
         self.camera.update()
         self.delta_time = self.clock.tick(FPS) / 1000
         pg.display.set_caption(f'{self.clock.get_fps() :.1f}')
 
-    def draw(self): 
+    def draw(self):
         """Draw all game elements on the screen."""
         self.screen.fill('black')
         self.map.draw()
@@ -51,7 +53,7 @@ class Game:
                 self.current_res = self.screen.get_size()
                 self.half_width = self.current_res[0] // 2
                 self.half_height = self.current_res[1] // 2
-    
+
     def run(self):
         """Run the game loop."""
         while True:
@@ -64,4 +66,3 @@ class Game:
 if __name__ == '__main__':
     game = Game()
     game.run()
-
