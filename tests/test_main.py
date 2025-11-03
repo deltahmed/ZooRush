df9dc8d36601273707cb322b84b708941f7b151e
Raphaël <twizox@gmail.com>
2025-11-03 14:25:34 +0100

    add test_main.py

---
+++ tests/test_main.py
@@ -0,0 +1,10 @@
+import pytest
+from Main import Game
+
+
+def test_game_update():
+    """Test one update cycle."""
+    game = Game()
+    old_time = game.delta_time
+    game.update()
+    assert game.delta_time != old_time
